23ccd7b40b3ac7e397e6e0e1e9d940790a89698e
Louis Law <louiscklaw@users.noreply.github.com>
2022-08-15 11:33:33 +0800

    update,

---
+++ frontend/components/TempForm.js
@@ -0,0 +1,30 @@
+;<form onSubmit={formik.handleSubmit}>
+  <Box display="flex" textAlign="center" justifyContent="center" flexDirection="column">
+    {/* <ImageAvatar size='md' src={avatarPreview || user?.avatar} /> */}
+
+    <Typography variant="body2">Max file size 50Mb</Typography>
+
+    <Button variant="contained" component="label">
+      Choose STL
+      <input
+        name="avatar"
+        accept="application/octet-stream"
+        id="contained-button-file"
+        type="file"
+        hidden
+        onChange={e => {
+          const fileReader = new FileReader()
+          fileReader.onload = () => {
+            if (fileReader.readyState === 2) {
+              formik.setFieldValue('stl_file', fileReader.result.split(',')[1])
+              setAvatarPreview(fileReader.result)
+            }
+          }
+          fileReader.readAsDataURL(e.target.files[0])
+        }}
+      />
+    </Button>
+
+    <Button type="submit">Upload</Button>
+  </Box>
+</form>
