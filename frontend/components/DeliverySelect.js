77606d618063ec3c15d6a16f365945aab5d824e2
louiscklaw <louiscklaw@gmail.com>
2022-09-17 03:12:58 +0800

    update and test,

--- frontend/components/DeliverySelect.js
+++ frontend/components/DeliverySelect.js
@@ -4,20 +4,20 @@ import FormControl from '@mui/material/FormControl';
 import MenuItem from '@mui/material/MenuItem';
 import Select from '@mui/material/Select';
 import * as React from 'react';
+import DELIVERY_OPTIONS from '../constants/delivery_options';
 
 export default function DeliverySelect({ formik }) {
-  const [age, setAge] = React.useState(0);
-
   const handleChange = event => {
-    setAge(event.target.value);
+    formik.setFieldValue('delivery', event.target.value);
   };
 
   return (
     <Box sx={{ minWidth: 120 }}>
       <FormControl sx={{ m: 1, minWidth: 120 }} fullWidth>
-        <Select value={age} onChange={handleChange} displayEmpty inputProps={{ 'aria-label': 'Without label' }}>
-          <MenuItem value={0}>觀塘站／地鐵綠色線交收</MenuItem>
-          <MenuItem value={1}>其他地點</MenuItem>
+        <Select value={delivery} onChange={handleChange} displayEmpty inputProps={{ 'aria-label': 'Without label' }}>
+          <MenuItem value={DELIVERY_OPTIONS.KWUN_TONG_MTR}>觀塘站</MenuItem>
+          <MenuItem value={DELIVERY_OPTIONS.GREEN_LINE}>地鐵綠色線交收</MenuItem>
+          <MenuItem value={DELIVERY_OPTIONS.ANYWHERE_ELSE}>其他地點</MenuItem>
         </Select>
         <FormHelperText>Without label</FormHelperText>
       </FormControl>
---
+++ frontend/constants/delivery_options.js
@@ -0,0 +1,5 @@
+export default {
+  KWUN_TONG_MTR: 0,
+  GREEN_LINE: 1,
+  ANYWHERE_ELSE: 2,
+};
